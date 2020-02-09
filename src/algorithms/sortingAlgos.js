03c247de63f9d343b0689c1a2e6d0cc4ae34beb1
Roberto Battaglia <battaroberto@gmail.com>
2020-02-09 22:02:37 +0000

    test mergeSorting algorithm

---
+++ src/algorithms/sortingAlgos.js
@@ -0,0 +1,28 @@
+export function mergeSort(unsortedArray) {
+  if (unsortedArray.length <= 1) {
+    return unsortedArray;
+  }
+  const middle = Math.floor(unsortedArray.length / 2);
+  const left = unsortedArray.slice(0, middle);
+  const right = unsortedArray.slice(middle);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(leftArray, rightArray) {
+  let resultArray = [];
+  let leftIdx = 0, rightIdx = 0;
+
+  while (leftIdx < leftArray.length && rightIdx < rightArray.length) {
+    if (leftArray[leftIdx] < rightArray[rightIdx]) {
+      resultArray.push(leftArray[leftIdx]);
+      leftIdx++;
+    } else {
+      resultArray.push(rightArray[rightIdx]);
+      rightIdx++;
+    }
+  }
+  return resultArray
+    .concat(leftArray.slice(leftIdx))
+    .concat(rightArray.slice(rightIdx));
+}
--- src/components/Sorter.jsx
+++ src/components/Sorter.jsx
@@ -1,4 +1,7 @@
 import React, {useState, useEffect} from 'react';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import * as sortingAlgos from '../algorithms/sortingAlgos';
 import './Sorter.css';
 
 const PRIMARY_COLOR = 'turquoise';
@@ -15,14 +18,29 @@ export default function Sorter() {
   const resetArray = () => {
     const newValues = [];
     for (let i = 0; i <= 300; ++i) {
-      newValues.push(randomIntFromInterval(5, 1000));
+      newValues.push(randomIntFromInterval(5, 700));
     }
-    setValues(newValues);
+    setValues(newValues)
+  }
+
+  const mergeSort = () => {
+    const jsSortedArray = values.slice().sort((v1, v2) => v1 - v2);
+    const mySortedArray = sortingAlgos.mergeSort(values);
+
+    console.log(arraysAreEqual(jsSortedArray, mySortedArray));
   }
 
   return (
     <>
       <div className="array-container">
+        <div className="bar">
+          <Button onClick={() => resetArray()} variant="contained" color="primary">Generate new array</Button>
+          <ButtonGroup variant="contained" color="primary" aria-label="contained primary button group">
+            <Button onClick={() => mergeSort()}>Merge Sort</Button>
+            <Button>Quick Sort</Button>
+            <Button>Something else sort</Button>
+          </ButtonGroup>
+        </div>
         {values.map((value, idx) => (
           <div
             className="array-bar"
@@ -39,4 +57,14 @@ export default function Sorter() {
 
 function randomIntFromInterval(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function arraysAreEqual(arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) return false;
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }
+  return true;
 }
