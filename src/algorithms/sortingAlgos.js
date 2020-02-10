8fe77b1167048f5e13da898307e3de269c7d9bd2
Roberto Battaglia <battaroberto@gmail.com>
2020-02-10 22:25:18 +0000

    developed animations for mergesorting

--- src/algorithms/sortingAlgos.js
+++ src/algorithms/sortingAlgos.js
@@ -1,8 +1,60 @@
 export function getMergeSortAnimations(unsortedArray) {
-  
+  const animations = [];
+  if (unsortedArray.length <= 1) return unsortedArray;
+  const auxiliaryArray = unsortedArray.slice();
+  mergeSortHelper(unsortedArray, 0, unsortedArray.length - 1, auxiliaryArray, animations);
+  return animations;
 }
 
+function mergeSortHelper(
+  mainArray,
+  startIdx,
+  endIdx,
+  auxiliaryArray,
+  animations
+) {
+  if (startIdx === endIdx) return;
+  const middleIdx = Math.floor((startIdx + endIdx) / 2);
+  mergeSortHelper(auxiliaryArray, startIdx, middleIdx, mainArray, animations);
+  mergeSortHelper(auxiliaryArray, middleIdx + 1, endIdx, mainArray, animations);
+  doMerge(mainArray, startIdx, middleIdx, endIdx, auxiliaryArray, animations);
+}
 
+function doMerge(
+  mainArray,
+  startIdx,
+  middleIdx,
+  endIdx,
+  auxiliaryArray,
+  animations
+) {
+  let k = startIdx;
+  let i = startIdx;
+  let j = middleIdx + 1;
+  while (i <= middleIdx && j <= endIdx) {
+    animations.push([i, j]);
+    animations.push([i, j]);
+    if (auxiliaryArray[i] <= auxiliaryArray[j]) {
+      animations.push([k, auxiliaryArray[i]]);
+      mainArray[k++] = auxiliaryArray[i++];
+    } else {
+      animations.push([k, auxiliaryArray[j]]);
+      mainArray[k++] = auxiliaryArray[j++];
+    }
+  }
+  while (i <= middleIdx) {
+    animations.push([k, i]);
+    animations.push([k, i]);
+    animations.push([k, auxiliaryArray[i]]);
+    mainArray[k++] = auxiliaryArray[i++];
+  }
+  while (j <= endIdx) {
+    animations.push([k, j]);
+    animations.push([k, j]);
+    animations.push([k, auxiliaryArray[j]]);
+    mainArray[k++] = auxiliaryArray[j++];
+  }
+}
 
 export function mergeSort(unsortedArray) {
   if (unsortedArray.length <= 1) {
--- src/components/Sorter.jsx
+++ src/components/Sorter.jsx
@@ -1,12 +1,16 @@
-import React, {setState} from 'react';
+import React from 'react';
 import Button from '@material-ui/core/Button';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 import * as sortingAlgos from '../algorithms/sortingAlgos';
 import './Sorter.css';
 
-const PRIMARY_COLOR = 'turquoise';
+const NEUTRAL_COLOR = 'pink';
+const PRIMARY_COLOR = 'deepskyblue';
+const SECONDARY_COLOR = 'red';
 
-const SORTED_COLOR = 'green';
+const SORTED_COLOR = 'springgreen';
+
+const ANIMATION_SPEED_MS = 10;
 
 export default class Sorter extends React.Component {
   constructor(props) {
@@ -14,6 +18,8 @@ export default class Sorter extends React.Component {
 
     this.state = {
       values: [],
+      valuesSize: 50,
+      stop: true
     };
   }
 
@@ -23,34 +29,73 @@ export default class Sorter extends React.Component {
 
   resetArray() {
     const newValues = [];
-    for (let i = 0; i <= 300; ++i) {
+    for (let i = 0; i <= this.state.valuesSize; ++i) {
       newValues.push(randomIntFromInterval(5, 700));
     }
+    this.setState({stop: true});
     this.setState({values: newValues}, () => this.resetArrayBarsColor());
   }
 
   resetArrayBarsColor() {
     const arrayBars = document.getElementsByClassName('array-bar');
     for (let i = 0; i < arrayBars.length; i++) {
-      arrayBars[i].style.backgroundColor = PRIMARY_COLOR;
+      arrayBars[i].style.backgroundColor = NEUTRAL_COLOR;
     }
+  }
+
+  sort(sortFunction) {
+    this.setState({stop: false}, () => sortFunction())
   }
 
   mergeSort() {
     const jsSortedArray = this.state.values.slice().sort((v1, v2) => v1 - v2);
-    const mySortedArray = sortingAlgos.mergeSort(this.state.values);
-    this.setState({values: mySortedArray}, () => this.colorSortedElements(jsSortedArray));
+    const animations = sortingAlgos.getMergeSortAnimations(this.state.values);
+    console.log(animations)
+    for (let i = 0; i < animations.length && !this.state.stop; i++) {
+      const arrayBars = document.getElementsByClassName('array-bar');
+      const isColorChange = i % 3 !== 2;
+      if (isColorChange) {
+        const [barOneIdx, barTwoIdx] = animations[i];
+        const barOneStyle = arrayBars[barOneIdx].style;
+        const barTwoStyle = arrayBars[barTwoIdx].style;
+        const color1 = i % 3 === 0 ? PRIMARY_COLOR : NEUTRAL_COLOR;
+        const color2 = i % 3 === 0 ? SECONDARY_COLOR : NEUTRAL_COLOR;
+        setTimeout(() => {
+          barOneStyle.backgroundColor = color1;
+          barTwoStyle.backgroundColor = color2;
+          this.colorElementIfSorted(jsSortedArray[barOneIdx], arrayBars[barOneIdx]);
+          this.colorElementIfSorted(jsSortedArray[barTwoIdx], arrayBars[barTwoIdx]);
+        }, i * ANIMATION_SPEED_MS);
+      } else {
+        setTimeout(() => {
+          const [barOneIdx, newValue] = animations[i];
+          const barOneStyle = arrayBars[barOneIdx].style;
+          barOneStyle.height = `${this.getHeight(newValue)}px`;
+          this.colorElementIfSorted(jsSortedArray[barOneIdx], arrayBars[barOneIdx]);
+        }, i * ANIMATION_SPEED_MS);
+      }
+    }
+  }
 
+  colorElementIfSorted(correctValue, element) {
+    if (correctValue == element.style.height.slice(0, -2)) {
+      setTimeout(() => {
+        element.style.backgroundColor = SORTED_COLOR;
+      }, 1);
+    }
+  }
+
+  getHeight(value) {
+    return value;
   }
 
   colorSortedElements(jsSortedArray) {
     const arrayBars = document.getElementsByClassName('array-bar');
     for (let i = 0; i < jsSortedArray.length; i++) {
-      let height = arrayBars[i].style.height;
-      if (jsSortedArray[i] == height.slice(0, -2)) {
+      if (jsSortedArray[i] == arrayBars[i].getAttribute('value')) {
         setTimeout(() => {
           arrayBars[i].style.backgroundColor = SORTED_COLOR;
-        }, i*2);
+        }, i*ANIMATION_SPEED_MS);
       }
     }
   }
@@ -62,7 +107,7 @@ export default class Sorter extends React.Component {
           <div className="bar">
             <Button onClick={() => this.resetArray()} variant="contained" color="primary">Generate new array</Button>
             <ButtonGroup variant="contained" color="primary" aria-label="contained primary button group">
-              <Button onClick={() => this.mergeSort()}>Merge Sort</Button>
+              <Button onClick={() => this.sort(this.mergeSort.bind(this))}>Merge Sort</Button>
               <Button>Quick Sort</Button>
               <Button>Something else sort</Button>
             </ButtonGroup>
@@ -70,11 +115,12 @@ export default class Sorter extends React.Component {
           {this.state.values.map((value, idx) => (
             <div
               className="array-bar"
+              value={value}
               key={idx}
               style={{
-                backgroundColor: PRIMARY_COLOR,
-                height: `${value}px`,
-                fontSize: "0px"
+                backgroundColor: NEUTRAL_COLOR,
+                height: `${this.getHeight(value)}px`,
+                width: `${500/this.state.values.length}px`
             }}/>
           ))}
         </div>
